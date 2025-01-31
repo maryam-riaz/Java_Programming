30b3e6c0839c3bf49318a7fe426e257b6ff78841
Maryam Riaz <maryamriaz301@gmail.com>
2025-01-31 20:52:29 +0500

    Escape Sequences added

---
+++ EscapeSequences.java
@@ -0,0 +1,14 @@
+public class EscapeSequences {
+	public static void main(String[] args)
+	{
+		System.out.println("Newline - \\n : \n ...");
+		System.out.println("Horizontal tab - \\t : \t ...");
+		System.out.println("Carriage return - \\r : \r ...");
+		System.out.println("Backslash - \\\\ : \\ ...");
+		System.out.println("Double quote - \\\" : \" ...");
+		System.out.println("Single quote - \\\' : \' ...");
+		System.out.println("Backspace - \\b : \b ...");
+		System.out.println("Form Feed - \\f : \f ...");
+	}
+	
+}
--- FirstJavaApplication.java
+++ FirstJavaApplication.java
@@ -1,6 +1,10 @@
 public class FirstJavaApplication{
 	public static void main(String[] args)
 	{
+
+		/*
+		 Difference between println and print
+		 */
 		System.out.println("Welcome to Java Programming!"); 	// println moves the cursor to the next line
 		System.out.print("Hello"); 							// print does not move the cursor to the next line
 		System.out.print(" World!");
