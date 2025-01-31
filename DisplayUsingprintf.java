5956975f96cdfdf8d136028123435f55afc2d428
Maryam Riaz <maryamriaz301@gmail.com>
2025-01-31 22:01:21 +0500

    using printf same as in C language

---
+++ DisplayUsingprintf.java
@@ -0,0 +1,8 @@
+public class DisplayUsingprintf{
+	public static void main(String[] args){
+		System.out.printf("%s%n%s%nHi", "Welcome to ", "Java Programming!");
+
+		// %s is a placeholder/format-specifier for string
+		// %n is the format specifier for line separator 
+	}
+}
