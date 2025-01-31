823d2f1d6c696573f0698625bdf473a40747ae86
Maryam Riaz <maryamriaz301@gmail.com>
2025-01-31 20:35:52 +0500

    difference btw println and print

--- FirstJavaApplication.java
+++ FirstJavaApplication.java
@@ -1,6 +1,8 @@
 public class FirstJavaApplication{
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Java Programming!");
+		System.out.println("Welcome to Java Programming!"); 	// println moves the cursor to the next line
+		System.out.print("Hello"); 							// print does not move the cursor to the next line
+		System.out.print(" World!");
 	}
 }
