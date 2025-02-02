67068502e9e5aaab454c5206eacf58f9597f8390
maryam-riaz <maryamriaz301@gmail.com>
2025-02-02 16:13:10 +0500

    dialog boxes

--- Account.java
+++ Account.java
@@ -1,19 +1,32 @@
 public class Account {
 	private String name; // instance variable/attribute //can be placed anywhere within the class,
 							// outside any method
+	private double balance;
 
+	// constructor
+	public Account(){}; //this declaration makes the use of default constructor possible in the client class
+	public Account(String name, double balance) {
+		this.name = name;
+		if (balance > 0.0)
+		this.balance = balance;
+	}
 	// method to set name
 	public void setName(String n) {
 		name = n;
 	}
-
-	// constructor
-	public Account(String name) {
-		this.name = name;
-	}
-
 	// method to retrieve name
 	public String getName() {
 		return name;
 	}
+	//balance getter 
+	public double getBalance(){
+		return balance;
+	}
+	//deposit 
+	public void deposit(double depositAmount){
+		if(depositAmount>0.0)
+			balance += depositAmount;
+	}
+
+
 } // end of Account class
--- AccountTest.java
+++ AccountTest.java
@@ -1,17 +1,20 @@
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class AccountTest {
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
 
-		//Account myAccount = new Account();		// use of default constructor 
+		//Account myAccount1 = new Account();		// use of default constructor 
 
-		System.out.print("Enter name: ");
-		Account myAccount = new Account(input.nextLine()); // use of parameterized constructor
+		System.out.print("Enter name and balance: ");
+		Account myAccount = new Account(input.nextLine(), input.nextDouble()); // use of parameterized constructor
 
 		//myAccount.setName(input.nextLine());
 
 		System.out.println("Account name: "+ myAccount.getName());
+		System.out.println("Account balance: "+ BigDecimal.valueOf(myAccount.getBalance()));
+
 
 		input.close();
 	}
